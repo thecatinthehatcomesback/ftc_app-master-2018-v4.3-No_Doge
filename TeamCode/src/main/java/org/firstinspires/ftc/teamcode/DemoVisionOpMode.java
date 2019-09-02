b3c9a02b792332baaf9f9012133cccf0ab921d7f
Cat <catcomesback10273@gmail.com>
2019-09-02 17:19:59 -0500

    Lots of Machine Vision work...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoVisionOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoVisionOpMode.java
@@ -9,10 +9,11 @@
  */
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -27,7 +28,7 @@ import static org.firstinspires.ftc.robotcore.external.tfod.TfodRoverRuckus.LABE
 import static org.firstinspires.ftc.robotcore.external.tfod.TfodRoverRuckus.TFOD_MODEL_ASSET;
 
 
-@Autonomous(name="Intermediate Autonomous", group="CatAuto")
+@TeleOp(name="DemoVision Autonomous", group="CatTeleOp")
 public class DemoVisionOpMode extends LinearOpMode {
 
     /* Declare OpMode members. */
@@ -44,7 +45,19 @@ public class DemoVisionOpMode extends LinearOpMode {
     public  TFObjectDetector tfod;
 
     // Variables
-    int goldMineralX = 0;
+    int goldMineralXPos     = 0;
+    int goldMineralWidth    = 0;
+
+    double forwardPower = 0.20;
+    double leftPower    = 0.00;
+    double rightPower   = 0.00;
+
+    boolean needToDriveAhead    = true;
+    boolean needToTurn          = false;
+    boolean needToTurnLeft      = false;
+    boolean needToTurnRight     = false;
+
+
 
 
     @Override
@@ -52,6 +65,8 @@ public class DemoVisionOpMode extends LinearOpMode {
         // Send telemetry message to signify robot is getting ready
         telemetry.addData("Status: ", "Initializing...");
         telemetry.update();
+
+        hwMap = hardwareMap;
 
         // Init the Drive Train
         initDrive();
@@ -68,60 +83,155 @@ public class DemoVisionOpMode extends LinearOpMode {
          * Runs after hit start:
          * DO STUFF FOR the OPMODE!!!
          */
+        ElapsedTime timer = new ElapsedTime();
+
         while (opModeIsActive()) {
             List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
 
             if (updatedRecognitions != null) {
                 for (Recognition recognition : updatedRecognitions) {
+                    // Lots of different tfod values placed into Telemetry
+                    /*telemetry.addData("getLabel", recognition.getLabel());
+                    telemetry.addData("getConfidence", recognition.getConfidence());
+                    telemetry.addData("getLeft", recognition.getLeft());
+                    telemetry.addData("getRight", recognition.getRight());
+                    telemetry.addData("getWidth", recognition.getWidth());
+                    telemetry.addData("estimateAngleToObject", recognition.estimateAngleToObject(AngleUnit.DEGREES));*/
+
                     if (recognition.getLabel().equals(LABEL_GOLD_MINERAL) && recognition.getConfidence() > 0.5) {
-                        int goldMineralX = (int) recognition.getLeft();
-                        // Look for the Gold Pos and decide which side of the sampling field the gold lies
-                        if (goldMineralX > 450) {
-                            //***Inverted this since the camera was recently placed upside down***//
-                            // Drive one way
-                            telemetry.addData("Drive", "One way");
+                        // Define the goldMineral-variables.
+                        goldMineralXPos = (int) recognition.getLeft();
+                        goldMineralWidth = (int) recognition.getWidth();
+
+                        /**
+                         * SET FLAGS!
+                         */
+                        // Look for Gold and decide which side of the sampling field the Gold is.
+                        if (goldMineralXPos > 200) {
+                            needToTurnLeft = true;
+                        } else {
+                            needToTurnLeft = false;
                         }
-                        // Drive the other way
-                        telemetry.addData("Drive", "Other way");
+                        if (goldMineralXPos < 400) {
+                            needToTurnRight = true;
+                        } else {
+                            needToTurnRight = false;
+                        }
+                        // If between these values (deadzone in the middle of the robot's view),
+                        // the robot doesn't need to turn and can drive ahead.
+                        if (needToTurnLeft && needToTurnRight) {
+                            needToTurn = false;
+                        } else {
+                            // Otherwise, the  robot must start turning to line up with Gold.
+                            needToTurn = true;
+                        }
+                        // Decide whether the robot needs to drive ahead.
+                        if (goldMineralWidth < 200) {
+                            needToDriveAhead = true;
+                        } else {
+                            needToDriveAhead = false;
+                        }
+                        timer.reset();
                     }
                 }
             }
-            telemetry.addData("goldMineralX", goldMineralX);
-            telemetry.update();
-        }
 
+        telemetry.addData("goldMineralXPos", goldMineralXPos);
+        telemetry.addData("goldMineralWidth", goldMineralWidth);
+        telemetry.addData("needToDriveAhead", needToDriveAhead);
+        telemetry.addData("needToTurn", needToTurn);
+        telemetry.update();
+        telemetry.addData("forwardPower", forwardPower);
+        telemetry.addData("leftPower", leftPower);
+        telemetry.addData("rightPower", rightPower);
         /**
-         * Plans...
+         * Plans:
          *
          * When it sees the Gold, it will chase it by:
-         * Setting powers to each side in order to line up with the Gold.
+         * Giving a constant +power to each side in order to get closer to the Gold.
          *
          * If it doesn't see the gold it will scan for it by:
          * Turning in a set direction somewhat slowly.
+         *
+         * Greater than 200 getWidth = stop
+         *
+         * if needToDriveAhead then drive ahead with one set of left/right difference powers
+         * otherwise use another set of left/right difference powers to turn the bot.
          */
+
+        // If the robot can't see the Gold, just stop and rest until it sees it again.
+        if (timer.seconds() > 0.25) {
+            needToDriveAhead = false;
+            needToTurn = false;
+        }
+
+        if (needToDriveAhead && !needToTurn) {
+            // Just drive  straight
+            forwardPower = 0.30;
+            leftPower = forwardPower;
+            rightPower = forwardPower;
+        } else if (needToDriveAhead && needToTurn) {
+            // Add/Subtract some power
+            forwardPower = 0.30;
+            if (needToTurnRight) {
+                leftPower = forwardPower + 0.15;
+                rightPower = forwardPower - 0.15;
+            } else {
+                leftPower = forwardPower - 0.15;
+                rightPower = forwardPower + 0.15;
+            }
+        } else if (!needToDriveAhead && needToTurn) {
+            // Add/Subtract MORE power
+            if (needToTurnRight) {
+                leftPower =   0.25;
+                rightPower = -0.25;
+            } else {
+                leftPower =  -0.25;
+                rightPower =  0.25;
+            }
+        } else {
+            // Stop.
+            forwardPower = 0.00;
+            leftPower = forwardPower;
+            rightPower = forwardPower;
+        }
+
+        // DRIVE //
+        leftFrontMotor.setPower(leftPower);
+        leftRearMotor.setPower(leftPower);
+        rightFrontMotor.setPower(rightPower);
+        rightRearMotor.setPower(rightPower);
+        }
     }
     /* Initialize standard Hardware interfaces */
     public void initDrive()  throws InterruptedException  {
 
         // Define and Initialize Motors //
         leftFrontMotor   = hwMap.dcMotor.get("left_front_motor");
-        rightFrontMotor  = hwMap.dcMotor.get("right_front_motor");
         leftRearMotor    = hwMap.dcMotor.get("left_rear_motor");
+        rightFrontMotor  = hwMap.dcMotor.get("right_front_motor");
         rightRearMotor   = hwMap.dcMotor.get("right_rear_motor");
 
 
         // Define motor directions //
         leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         leftRearMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
 
 
+        // Define motor modes //
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRearMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRearMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
         // Set all motors to run at no power so that the robot doesn't move during init //
-        leftFrontMotor.setPower(0);
-        rightRearMotor.setPower(0);
-        leftRearMotor.setPower(0);
-        rightRearMotor.setPower(0);
+        leftFrontMotor.setPower(0.00);
+        leftRearMotor.setPower(0.00);
+        rightRearMotor.setPower(0.00);
+        rightRearMotor.setPower(0.00);
     }
     public void initVision() {
 
